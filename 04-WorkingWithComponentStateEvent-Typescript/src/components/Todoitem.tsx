2244da7f5dd5e58f5f569147611dbc43904b85a0
hungnd2905 <hungnd2905@gmail.com>
2022-05-31 23:16:56 +0200

    working with component state event with Typescript

---
+++ 04-WorkingWithComponentStateEvent-Typescript/src/components/Todoitem.tsx
@@ -0,0 +1,8 @@
+const TodoItem: React.FC<{
+  text: string;
+  onRemoveTodo: (event: React.MouseEvent) => void;
+}> = (props) => {
+  return <li onClick={props.onRemoveTodo}>{props.text} </li>;
+};
+
+export default TodoItem;
